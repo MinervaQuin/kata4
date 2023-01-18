97d4d9ec3578322277aaa4dcd0d4ca5d8f2d021d
MinervaQuin <minervaqf8@gmail.com>
2023-01-18 17:34:17 +0000

    first commit

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kata3;
+
+/**
+ *
+ * @author Minerva
+ */
+public class Kata3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Histogram<String> histo= new Histogram();
+        histo.increment("ulpgc.es");
+        histo.increment("gmail.es");
+        histo.increment("prontonmail.es");
+         histo.increment("prontonmail.es");
+          histo.increment("prontonmail.es"); histo.increment("prontonmail.es");
+          histo.increment("ulpgc.es");histo.increment("ulpgc.es");histo.increment("ulpgc.es");histo.increment("ulpgc.es");
+          
+        new HistogramDisplay(histo).execute();
+    }
+    
+}
